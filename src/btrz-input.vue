695cf703d3986d1e8d0353e86496333eca10c0ef
Fack <fblind@gmail.com>
2017-03-16 17:05:37 -0300

    Show errors and add dirty state for btrz input

--- src/btrz-input.vue
+++ src/btrz-input.vue
@@ -6,7 +6,7 @@
       :placeholder="placeholder"
       v-validate
       :data-vv-rules="validations" />
-    <div v-show="errors.has('inputValidate')" class="error">{{ errors.first('inputValidate') }}</div>
+    <div v-show="fields.dirty(this.name) && errors.has(this.name)" class="error">{{ errors.first(this.name) }}</div>
   </div>
 </template>
 
