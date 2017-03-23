23a9c5abcf6c484405eb340e589b178248a833aa
Alejandro Cirino <cirinoalejandro@gmail.com>
2017-03-23 09:42:42 -0300

    Add btrz-lang-selector component

---
+++ src/btrz-lang-selector.vue
@@ -0,0 +1,33 @@
+<template v-if="languages && Object.keys(languages).length > 1">
+  <div class="flex-item user-nav text-center">
+    <span class="language-switcher">
+      <select :value="lang" v-on:change="changeLang($event.target.value)">
+        <option v-if="languages.en" value="en">English</option>
+        <option v-if="languages.fr" value="fr">Français</option>
+      </select>
+    </span>
+  </div>
+</template>
+
+<script>
+  //usage: <btrz-lang-selector :languages="languages" v-model="lang"></btrz-lang-selector>
+  export default {
+    name: "BtrzLangSelector",
+    model: {
+      prop: 'language',
+      event: 'change'
+    },
+    props: {
+      "languages": Object,
+      "language": String,
+    },
+    methods: {
+      changeLang: function (val) {
+        this.$emit("change", val);
+      }
+    },
+    data () {
+      return {lang: this.language};
+    }
+  };
+</script>
