8199ce432ab678427ebd7d1280ebaa781b9e202b
Damian Mantuano <smarbos@gmail.com>
2017-05-09 15:24:26 -0300

    restored btrz-input code

--- src/btrz-input.vue
+++ src/btrz-input.vue
@@ -1,18 +1,18 @@
 <template>
-    <input class="form-control full-width"
+    <input class="form-control"
       :id="id"
       :name="name"
       :type="type"
       :value="value"
       :placeholder="placeholder"
-      @input="$emit('input', $event.target.value);"
-      @blur="$emit('blur', $event.target.value);"
-      @focus="$emit('focus', $event.target.value);"/>
+      @input="$emit('input', $event.target.value)"
+      @blur="$emit('blur', $event.target.value)"
+      @focus="$emit('focus', $event.target.value)"/>
 </template>
 
 <script>
   export default {
     name: "BtrzInput",
-    props: ["placeholder", "type", "name", "value", "id"]
+    props: ["placeholder", "type", "name", "value"]
   }
 </script>
