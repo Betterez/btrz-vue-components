4416ae66261f31e399b563620161df174d04bdfc
Damian Mantuano <smarbos@gmail.com>
2017-05-09 15:27:11 -0300

    added btrz-filter-row and btrz-filter-col

---
+++ src/btrz-filter-col.vue
@@ -0,0 +1,29 @@
+<template>
+  <div :class="colClass">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "BtrzFilterCol",
+    props: {
+      colsize: {
+        type: Array,
+        default: () => []
+      }
+    },
+    computed: {
+      colClass: function() {
+        var colClass = "";
+        for (var i = 0; i < this.colsize.length; i++) {
+          colClass += "col-"+this.colsize[i];
+          if(i <= this.colsize.length-1){
+            colClass += " ";
+          }
+        }
+        return colClass;
+      }
+    }
+  }
+</script>
