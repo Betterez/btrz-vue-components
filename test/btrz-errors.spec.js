4ca922a87a6f77843dbf8d58cf959e16e3883d8b
Damian Mantuano <smarbos@gmail.com>
2017-05-17 18:10:11 -0300

    Added btrz-errors and btrz-label tests

---
+++ test/btrz-errors.spec.js
@@ -0,0 +1,35 @@
+import "./setup";
+
+import {expect} from "chai";
+import {insertHTML} from "./utils";
+import BtrzInput from "../src/btrz-input";
+import BtrzToggle from "../src/btrz-toggle";
+
+describe.only("BtrzLabel", () => {
+  it("btrz-input should render errors if a error object is given as parameter.", () => {
+    insertHTML(`<div id="app">
+                  <btrz-input type='text' class="form-control" :errors="[{'type': 0, 'message': 'Error message goes here'}, {'type': 0, 'message': 'Error message goes here'}]"></btrz-input>
+                </div>`);
+
+    const app = new Vue({
+        el: "#app",
+        components: {BtrzInput}
+      }),
+      errors = $(".form-control .error");
+    expect(errors.length).to.equal(2);
+  });
+
+  it("btrz-toggle should render errors if a error object is given as parameter.", () => {
+    insertHTML(`<div id="app">
+                  <btrz-toggle class="form-control" :errors="[{'type': 0, 'message': 'Error message goes here'}]" value="false"></btrz-toggle>
+                </div>`);
+
+    const app = new Vue({
+        el: "#app",
+        components: {BtrzToggle}
+      }),
+      errors = $(".form-control .error");
+    expect(errors.length).to.equal(1);
+  });
+
+});
---
+++ test/btrz-label.spec.js
@@ -0,0 +1,35 @@
+import "./setup";
+
+import {expect} from "chai";
+import {insertHTML} from "./utils";
+import BtrzInput from "../src/btrz-input";
+import BtrzToggle from "../src/btrz-toggle";
+
+describe("BtrzLabel", () => {
+  it("btrz-input should render a label with a given text", () => {
+    insertHTML(`<div id="app">
+                  <btrz-input type='text' class="form-control" label="test label"></btrz-input>
+                </div>`);
+
+    const app = new Vue({
+        el: "#app",
+        components: {BtrzInput}
+      }),
+      label = $(".form-control label");
+    expect(label.text()).to.equal("test label:");
+  });
+
+  it("btrz-toggle should render a label with a given text", () => {
+    insertHTML(`<div id="app">
+                  <btrz-toggle class="form-control" label="Activate this" value="false"></btrz-toggle>
+                </div>`);
+
+    const app = new Vue({
+        el: "#app",
+        components: {BtrzToggle}
+      }),
+      label = $(".form-control label");
+    expect(label.text()).to.equal("Activate this:");
+  });
+
+});
