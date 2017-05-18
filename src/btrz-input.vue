f60af3410d1ef6dd09a6845d167fe8bb950d8eeb
Martin Silva <msmartinsilva@gmail.com>
2017-05-18 13:34:10 -0300

    prefix and postfix for btrz-input

--- src/btrz-input.vue
+++ src/btrz-input.vue
@@ -5,16 +5,20 @@
       'input--filled': !isEmpty
     }">
     <btrz-label :id="id" :label="label"></btrz-label>
-    <input class="form-control"
-    :id="id"
-    :name="name"
-    :type="type"
-    :value="inputValue"
-    :placeholder="placeholder"
-    :label="label"
-    @input="valueUpdated($event.target.value);"
-    @blur="focusUpdated('blur', $event.target.value);"
-    @focus="focusUpdated('focus', $event.target.value);"/>
+    <div class="input-group">
+      <span v-if="prefix" class="input-group-addon">{{prefix}}</span>
+      <input class="form-control"
+      :id="id"
+      :name="name"
+      :type="type"
+      :value="inputValue"
+      :placeholder="placeholder"
+      :label="label"
+      @input="valueUpdated($event.target.value);"
+      @blur="focusUpdated('blur', $event.target.value);"
+      @focus="focusUpdated('focus', $event.target.value);"/>
+      <span v-if="postfix" class="input-group-addon">{{postfix}}</span>
+    </div>
     <btrz-errors :errors="errors"></btrz-errors>
   </div>
 </template>
@@ -28,7 +32,7 @@
       BtrzLabel,
       BtrzErrors
     },
-    props: ["placeholder", "type", "name", "value", "id", "label", "errors"],
+    props: ["placeholder", "type", "name", "value", "id", "label", "errors", "prefix", "postfix"],
     methods: {
       _updateValue(value) {
         this.inputValue = value;
