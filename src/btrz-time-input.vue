8ddf845bf979bf797aa8d7ca0eba9318d0e2e2fe
Damian Mantuano <smarbos@gmail.com>
2017-05-22 16:51:06 -0300

    Added btrz-time-input component

---
+++ src/btrz-time-input.vue
@@ -0,0 +1,50 @@
+<template>
+  <div :class="{
+      'input--focused': focused,
+      'input--empty': isEmpty,
+      'input--filled': !isEmpty
+    }">
+    <btrz-label :id="id" :label="label"></btrz-label>
+    <input class="clock-bkg form-control full-width"
+    :id="id"
+    :name="name"
+    :type="type"
+    :value="inputValue"
+    :placeholder="placeholder"
+    :label="label"
+    @input="valueUpdated($event.target.value);"
+    @blur="focusUpdated('blur', $event.target.value);"
+    @focus="focusUpdated('focus', $event.target.value);"/>
+    <btrz-errors :errors="errors"></btrz-errors>
+  </div>
+</template>
+
+<script>
+  import BtrzLabel from "../../btrz-vue-components/src/btrz-label";
+  import BtrzErrors from "../../btrz-vue-components/src/btrz-errors";
+  export default {
+    name: "BtrzInput",
+    components:{
+      BtrzLabel,
+      BtrzErrors
+    },
+    props: ["placeholder", "type", "name", "value", "id", "label", "errors"],
+    methods: {
+      valueUpdated(value) {
+        this.inputValue = value;
+        this.isEmpty = !Boolean(value);
+        this.$emit('input', value);
+      },
+      focusUpdated(focus, value) {
+        this.$emit(focus, value);
+        this.focused = !this.focused;
+      }
+    },
+    mounted() {
+      this.valueUpdated(this.value);
+    },
+    data() {
+      return {isEmpty: true, focused: false, inputValue: this.value};
+    }
+  }
+</script>
---
+++ test/btrz-time-input.spec.js
@@ -0,0 +1,34 @@
+import "./setup";
+
+import {expect} from "chai";
+import {insertHTML} from "./utils";
+import BtrzTimeInput from "../src/btrz-time-input";
+
+describe("BtrzTimeInput", () => {
+  it("Time input should contain a placeholder", () => {
+    insertHTML(`<div id="app">
+                  <btrz-time-input name="testName" type="testType" value="testValue" placeholder="hh:mm"></btrz-time-input>
+                </div>`);
+
+    const app = new Vue({
+        el: "#app",
+        components: {BtrzTimeInput}
+      }),
+    input = $("#app input");
+    expect(input.attr('placeholder')).to.equal("hh:mm");
+  });
+
+  it("Time input should contain a clock-bkg class", () => {
+    insertHTML(`<div id="app">
+                  <btrz-time-input name="testName" type="testType" value="testValue" placeholder="hh:mm"></btrz-time-input>
+                </div>`);
+
+    const app = new Vue({
+        el: "#app",
+        components: {BtrzTimeInput}
+      }),
+    input = $("#app input");
+    expect(input.hasClass('clock-bkg')).to.equal(true);
+  });
+
+});
