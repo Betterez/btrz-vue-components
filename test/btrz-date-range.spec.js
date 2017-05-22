fc48b975ddc4c8984d094a78061610e75743fac2
Damian Mantuano <smarbos@gmail.com>
2017-05-22 16:49:53 -0300

    updated date-range component tests

--- test/btrz-date-range.spec.js
+++ test/btrz-date-range.spec.js
@@ -1,14 +1,23 @@
 import "./setup";
 
 import {expect} from "chai";
+import {mount} from "avoriaz";
+import moment from "moment";
 import {insertHTML} from "./utils";
 import BtrzDateRange from "../src/btrz-date-range";
+
+const dummyLogger = {
+  warn() { return null; },
+  error() { return null; },
+  info() { return null; }
+};
+
 
 describe("BtrzDateRange", () => {
   it("Should render two btrz-date-input.", () => {
     insertHTML(`<div id="app">
                   <btrz-date-range
-                    id='travelDate' fro
+                    id='travelDate'
                     lang='en'
                     start-date-name='startDateName'
                     start-date-label='Start Date'
@@ -29,7 +38,7 @@ describe("BtrzDateRange", () => {
   it("Should use the given labels for start and end date input.", () => {
     insertHTML(`<div id="app">
                   <btrz-date-range
-                    id='travelDate' fro
+                    id='travelDate'
                     lang='en'
                     start-date-name='startDateName'
                     start-date-label='Start Date'
@@ -46,6 +55,63 @@ describe("BtrzDateRange", () => {
     label = $("#app label");
     expect(label.text().indexOf('Start Date')).to.not.equal(-1);
     expect(label.text().indexOf('End Date')).to.not.equal(-1);
+  });
+
+  it("End date picker Should initialize disabled", () => {
+    let wrapper = null;
+    const EXPECTED_DATE = new Date();
+    let props = {
+      value: EXPECTED_DATE,
+      startDateName: 'testStartDateName',
+      startDateLabel: 'testStartDateLabel',
+      endDateName: 'testEndDateName',
+      endDateLabel: 'testEndDateLabel',
+      lang: 'en'
+    };
+    wrapper = mount(BtrzDateRange, {
+      data: {logger: dummyLogger},
+      propsData: props
+    });
+    expect(wrapper.vm.$refs.endDate.disabled).to.equal(true);
+  });
+
+  it("End date picker should get enabled if start date is selected.", () => {
+    let wrapper = null;
+    const EXPECTED_DATE = new Date();
+    let props = {
+      value: EXPECTED_DATE,
+      startDateName: 'testStartDateName',
+      startDateLabel: 'testStartDateLabel',
+      endDateName: 'testEndDateName',
+      endDateLabel: 'testEndDateLabel',
+      lang: 'en'
+    };
+    wrapper = mount(BtrzDateRange, {
+      data: {logger: dummyLogger},
+      propsData: props
+    });
+    wrapper.vm.$refs.startDate.picker.set("select", EXPECTED_DATE);
+    expect(wrapper.vm.endDateDisabled).to.equal(false);
+  });
+
+  it("End date picker min value should be start date.", () => {
+    let wrapper = null;
+    const EXPECTED_DATE = new Date();
+    let props = {
+      value: EXPECTED_DATE,
+      startDateName: 'testStartDateName',
+      startDateLabel: 'testStartDateLabel',
+      endDateName: 'testEndDateName',
+      endDateLabel: 'testEndDateLabel',
+      lang: 'en'
+    };
+    wrapper = mount(BtrzDateRange, {
+      data: {logger: dummyLogger},
+      propsData: props
+    });
+    wrapper.vm.$refs.startDate.picker.set("select", EXPECTED_DATE);
+    const actualDate = wrapper.vm.$refs.startDate.picker.get("select").obj;
+    expect(moment(wrapper.vm.endDateMinValue).isSame(EXPECTED_DATE, "days")).to.be.ok;
   });
 
 });
