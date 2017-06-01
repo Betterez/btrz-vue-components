5c425d984b4c6481c4da11bcd2a92f8cff3165d2
Fack <facundo.soria@devecoop.com>
2017-06-01 12:03:37 -0300

    Remove the asignation of value

--- src/btrz-date-input.vue
+++ src/btrz-date-input.vue
@@ -124,11 +124,9 @@
           const date = this.formatDate(context.select);
           this.$emit("change", date);
           this.isEmpty = !Boolean(date);
-          this.value = date;
         } else if ("clear" in context) {
           this.$emit("change", null);
           this.isEmpty = !Boolean(null);
-          this.value = null;
         }
       },
       getTranslation(lang) {
