7c53700e172117e37dc6c8e4b07c93e7cced1f84
Germán Podestá <german.podesta@devecoop.com>
2017-07-06 16:54:38 -0300

    Improved webpack configuration

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require("webpack");
 
+// WARNING: This conf is only for testing
 
 module.exports = {
   resolve: {
@@ -20,14 +21,38 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "babel-loader",
+        options: {
+          cacheDirectory: true
+        }
       },
       {
         test: /\.vue$/,
-        loader: 'vue-loader',
+        loader: "vue-loader",
+        query: {
+          loaders:
+          {
+            js: "babel-loader?cacheDirectory",
+            css: "ignore-loader", // Ignore css for tests
+            less: "ignore-loader",
+            sass: "ignore-loader",
+            scss: "ignore-loader",
+            stylus: "ignore-loader",
+            styl: "ignore-loader"
+          },
+          preserveWhitespace: false
+        }
       },
       {
         test: /\.css$/,
-        use: ["css-loader"]
+        use: ["ignore-loader"]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/, // Leave the image like it was but don't copy the file
+        loader: "file-loader",
+        query: {
+          emitFile: false,
+          name: "[path][name].[ext]"
+        }
       }
     ]
   },
