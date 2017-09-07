cb4b13998975a1ef77f9f98ee3c5d28d941234bd
Martin Silva <msmartinsilva@gmail.com>
2017-09-07 11:58:04 -0300

    add click event on btrz-product-card

--- src/btrz-product-card.vue
+++ src/btrz-product-card.vue
@@ -1,5 +1,5 @@
 <template>
-  <a :href="`${baseUrl}/${product.family}/${product._id}`" class="ellipsis btn btn-lg btn-default btn-block btn-product">
+  <a v-on:click="propagateEvent" :href="`${baseUrl}/${product.family}/${product._id}`" class="ellipsis btn btn-lg btn-default btn-block btn-product">
     <i :class="`fa ${icon}`"></i> {{ $t(product.lexiconKeys.name) || product.name }}
     <p class="ellipsis hidden-xs">{{ $t(product.lexiconKeys.description) || product.description }}</p>
   </a>
@@ -9,6 +9,11 @@
 export default {
   name: "BtrzProductCard",
   props: ["product", "baseUrl"],
+  methods: {
+    propagateEvent($event) {
+      this.$emit("click", $event);
+    }
+  },
   computed: {
     icon() {
       return this.product.icon || "fa-ticket";
