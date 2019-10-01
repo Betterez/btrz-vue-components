c343081691343e490513c4783f9149c8978ab44c
diegodindurra <diego@betterez.com>
2019-10-01 11:37:35 -0300

    #152800. Make all reservations screens accessible for readers.

--- src/btrz-date-input.vue
+++ src/btrz-date-input.vue
@@ -26,7 +26,7 @@
 
   const pickadateI18nStrings = {
     es: {
-      monthsFull: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembere", "Octubre", "Novimbre", "Diciembre"],
+      monthsFull: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
       monthsShort: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"],
       weekdaysFull: ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"],
       weekdaysShort: ["Dom", "Lun", "Mar", "Mié", "Jue", "Vie", "Sáb"],
